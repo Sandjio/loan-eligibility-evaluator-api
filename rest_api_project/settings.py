44faf30c7e85ecde8318938324b4faedb8393611
Sandjio <emmasandjio@gmail.com>
2025-06-30 18:59:14 +0100

    Update database settings to use MySQL instead of SQLite

--- rest_api_project/settings.py
+++ rest_api_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "rest_api_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "john_doe",
+        "USER": "admin",
+        "PASSWORD": "strong_password123",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
